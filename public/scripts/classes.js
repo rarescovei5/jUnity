871b42291064009d8ef6d94acbe4efb5a3c67d2f
Covei Rares <rarescovei5@gmail.com>
2024-12-18 21:10:48 +0200

    Added obligatory scene object properties

--- public/scripts/classes.js
+++ public/scripts/classes.js
@@ -1,3 +1,19 @@
+class Transform {
+  constructor({ position, angle, scale }) {
+    this.position.x = position.x;
+    this.position.y = position.y;
+    this.rotation = rotation;
+    this.scale.x = scale.x;
+    this.scale.y = scale.y;
+  }
+}
+class SpriteRenderer {
+  constructor({ sprite, color }) {
+    this.sprite = sprite;
+    this.color = color;
+  }
+}
+
 class GameEngine {
   constructor(canvas) {
     let objects = {};
